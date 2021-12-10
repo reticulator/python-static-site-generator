217f301b1de7a9e022ee7405c35a88cff4081a79
Chuck Eyres <chuck.eyres@gmail.com>
2021-12-10 08:50:55 -0800

    Section 4 - Checkpoint 1

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,7 +1,11 @@
 import shutil
+import sys
 
 from typing import List
 from pathlib import Path
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 
 
 class Parser:
@@ -31,3 +35,15 @@ class ResourceParser(Parser):
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+  
+
