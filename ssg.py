755fd0491fb081177f4f63aa64d5cf33ba1ae0d9
Chuck Eyres <chuck.eyres@gmail.com>
2021-12-10 09:06:49 -0800

    Section 4 - Checkpoint 3

--- ssg.py
+++ ssg.py
@@ -8,8 +8,13 @@ def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": [ssg.parsers.ResourceParser(),],
+        "parsers": [
+            ssg.parsers.ResourceParser(), 
+            ssg.parsers.MarkdownParser(), 
+            ssg.parsers.ReStructuredTextParser()
+        ],
     }
+    
     Site(**config).build()
 
 
